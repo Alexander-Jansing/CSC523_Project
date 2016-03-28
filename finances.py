1d9d3780108f20a671d695cd9a1aa283be1d5e9e
alexander-jansing <jansina@sunyit.edu>
2016-03-28 18:36:29 -0400

    *replaces quick sort with a quick and dirty sort to sort 2d-list based on the first elements of each sub-list. *started second process

--- finances.py
+++ finances.py
@@ -12,7 +12,10 @@ def runMe(fileAndExtraMoney): #fileAndExtraMoney is an array holding a file and 
 				finances.append(line)
 		comm.send(finances, dest=1)
 		comm.send(finances, dest=2)
-		if rank == 1:  #PAY THE HIGHEST LONE OFF FIRST
+		
+		###PAY THE HIGHEST LONE OFF FIRST###
+		if rank == 1:
+			comm.recv(source=0)
 			sum = 0
 			interests = []
 			for i in range(len(finances)):
@@ -22,58 +25,36 @@ def runMe(fileAndExtraMoney): #fileAndExtraMoney is an array holding a file and 
 			while(sum != 0):
 				for i in range(len(finances)):
 					sum -= minimumPayment(finances)
+		
+		###PAY LOANS PROPORTIONALLY###
+		if rank == 2:
+			comm.recv(source=0)
+			sum = 0
+			while(sum != 0):
+				for i in range(len(finances)):
+					sum -= minimumPayment(finances)
+
 
 
 def sortInterests(interests):
-	if 
+	sortedInterests = []
+	while(len(interests) != 0):
+		pointOfInterest = interests[0]
+		if len(interests) > 1:
+			for i in range(1, len(interests)):
+				if pointOfInterest > interests[i]:
+					pointOfInterest = interests[i]
+		interests.remove(pointOfInterest)
+		sortInterests.append(pointOfInterest)
+	return sortedInterests;
+
 	return 0;
 def minimumPayment(finances):
 	return finances[0]*finances[1]+finances[0]*.01;
 
 	MPI.Finalize();
 
-##### QUICKSORT FROM http://interactivepython.org/runestone/static/pythonds/SortSearch/TheQuickSort.html ###
-def quickSort(alist):
-   quickSortHelper(alist,0,len(alist)-1)
 
-def quickSortHelper(alist,first,last):
-   if first<last:
-
-       splitpoint = partition(alist,first,last)
-
-       quickSortHelper(alist,first,splitpoint-1)
-       quickSortHelper(alist,splitpoint+1,last)
-
-
-def partition(alist,first,last):
-   pivotvalue = alist[first]
-
-   leftmark = first+1
-   rightmark = last
-
-   done = False
-   while not done:
-
-       while leftmark <= rightmark and alist[leftmark] <= pivotvalue:
-           leftmark = leftmark + 1
-
-       while alist[rightmark] >= pivotvalue and rightmark >= leftmark:
-           rightmark = rightmark -1
-
-       if rightmark < leftmark:
-           done = True
-       else:
-           temp = alist[leftmark]
-           alist[leftmark] = alist[rightmark]
-           alist[rightmark] = temp
-
-   temp = alist[first]
-   alist[first] = alist[rightmark]
-   alist[rightmark] = temp
-
-
-   return rightmark
-### QUICKSORT FROM http://interactivepython.org/runestone/static/pythonds/SortSearch/TheQuickSort.html ###
 
 
 
